0a7fcbefa676d8bbe05119f108df498f9c0e583a
lzl2040 <2040171586@qq.com>
2026-03-01 13:42:06 +0000

    update run.sh

---
+++ run.sh
@@ -0,0 +1,38 @@
+NNODES=1
+NPROC_PER_NODE=2
+
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --nnodes)
+            NNODES="$2"
+            shift 2
+            ;;
+        --nproc_per_node)
+            NPROC_PER_NODE="$2"
+            shift 2
+            ;;
+        --node_rank)
+            NODE_RANK="$2"
+            shift 2
+            ;;
+        --master_addr)
+            MASTER_ADDR="$2"
+            shift 2
+            ;;
+        --master_port)
+            MASTER_PORT="$2"
+            shift 2
+            ;;
+        *)
+            echo "未知参数: $1"
+            exit 1
+            ;;
+    esac
+done
+
+uv run --no-sync --extra cu128 --group libero \
+  torchrun --nproc_per_node=$NPROC_PER_NODE --master_port=$MASTER_PORT -m cosmos_policy.scripts.train \
+  --config=cosmos_policy/config/config.py -- \
+  experiment="cosmos_predict2_2b_480p_libero" \
+  trainer.grad_accum_iter=8

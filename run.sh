a1f300f16061dff8aae1611c1a591cca96a55e9f
lzl2040 <2040171586@qq.com>
2026-03-02 15:51:27 +0000

    update sh

--- run.sh
+++ run.sh
@@ -35,10 +35,11 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# export BASE_DATASETS_DIR="/home/cosmos/.cache/datasets"
-# export IMAGINAIRE_OUTPUT_ROOT="/home/cosmos/.cache/imaginaire_output"
+export BASE_DATASETS_DIR="/home/cosmos/.cache/datasets"
+export IMAGINAIRE_OUTPUT_ROOT="/home/cosmos/.cache/imaginaire_output"
 uv run --no-sync --extra cu128 --group libero --python 3.10 \
-  torchrun --nproc_per_node=$NPROC_PER_NODE --master_port=$MASTER_PORT -m cosmos_policy.scripts.train \
+  torchrun --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+  -m cosmos_policy.scripts.train \
   --config=cosmos_policy/config/config.py --job_name=$JOB_NAME -- \
   experiment="cosmos_predict2_2b_480p_libero" \
   trainer.grad_accum_iter=8

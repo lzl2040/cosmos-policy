0df5fddb60e32b5231b8b1ec3260b427bdbe63fa
lzl2040 <2040171586@qq.com>
2026-04-03 09:07:56 +0000

    fix

--- cosmos_policy/scripts/train.py
+++ cosmos_policy/scripts/train.py
@@ -76,7 +76,7 @@ def launch(config: Config, args: argparse.Namespace) -> None:
             sampler=sampler,
             batch_size=config.dataloader_train.batch_size,
             # drop_last=config.dataloader_train.drop_last,
-            num_workers=config.dataloader_train.num_workers,
+            # num_workers=config.dataloader_train.num_workers,
             # persistent_workers=config.dataloader_train.persistent_workers,
             pin_memory=config.dataloader_train.pin_memory,
             num_workers=2,

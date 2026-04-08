1c56c34ccf65a7d1d09fbc411c8eabcacb8326a9
lzl2040 <2040171586@qq.com>
2026-04-08 06:19:13 +0000

    update

--- cosmos_policy/_src/imaginaire/trainer.py
+++ cosmos_policy/_src/imaginaire/trainer.py
@@ -296,13 +296,14 @@ class ImaginaireTrainer:
         with distributed.ddp_sync_grad(model_ddp, grad_accum_iter == self.config.trainer.grad_accum_iter - 1):
             self.callbacks.on_before_forward(iteration=iteration)
             
-            output_batch, loss = model_ddp.training_step(data, iteration)
+            with torch.no_grad():
+                output_batch, loss = model_ddp.training_step(data, iteration)
             
             self.callbacks.on_after_forward(iteration=iteration)
             self.callbacks.on_before_backward(model_ddp, loss, iteration=iteration)
 
-            loss_scaled = grad_scaler.scale(loss / self.config.trainer.grad_accum_iter)
-            loss_scaled.backward()
+            # loss_scaled = grad_scaler.scale(loss / self.config.trainer.grad_accum_iter)
+            # loss_scaled.backward()
             if self.config.trainer.distributed_parallelism == "ddp":
                 model_ddp.module.on_after_backward()
             else:
@@ -315,15 +316,15 @@ class ImaginaireTrainer:
             self.callbacks.on_before_optimizer_step(
                 model_ddp, optimizer, scheduler, grad_scaler, iteration=iteration
             )
-            grad_scaler.step(optimizer)
-            grad_scaler.update()
-            scheduler.step()
+            # grad_scaler.step(optimizer)
+            # grad_scaler.update()
+            # scheduler.step()
             self.callbacks.on_before_zero_grad(model_ddp, optimizer, scheduler, iteration=iteration)
             if self.config.trainer.distributed_parallelism == "ddp":
                 model_ddp.module.on_before_zero_grad(optimizer, scheduler, iteration=iteration)
             else:
                 model_ddp.on_before_zero_grad(optimizer, scheduler, iteration=iteration)
-            optimizer.zero_grad(set_to_none=True)
+            # optimizer.zero_grad(set_to_none=True)
             grad_accum_iter = 0
         return output_batch, loss, grad_accum_iter
 

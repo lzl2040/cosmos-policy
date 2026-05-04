b167355787e68df2dfd84e602e76522592977679
lzl2040 <2040171586@qq.com>
2026-05-04 09:46:32 +0000

    update

--- cosmos_policy/datasets/lerobot/mixtures.py
+++ cosmos_policy/datasets/lerobot/mixtures.py
@@ -411,6 +411,7 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         ("cup_4hz", 1.0),
         ("block_full", 1.0),
         ("sauce_full", 1.0),
+        ("pizza_sub1", 1.0),
         ("pizza_sub2", 1.0)
     ],
 

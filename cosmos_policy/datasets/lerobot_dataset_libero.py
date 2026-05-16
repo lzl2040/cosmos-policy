5672851ef38d9079e38b442aa08819ecb2552d80
lzl2040 <2040171586@qq.com>
2026-05-16 05:19:23 +0000

    fix bug

--- cosmos_policy/datasets/lerobot_dataset_libero.py
+++ cosmos_policy/datasets/lerobot_dataset_libero.py
@@ -1607,7 +1607,13 @@ class MultiDatasetforDistTraining(torch.utils.data.Dataset):
             # print(task_embeddings.shape)
             # task_embeddings = torch.squeeze(torch.zeros((1, 512, 1024)))
         else:
-            task_embeddings = torch.squeeze(self.t5_text_embeddings[item["task"]])
+            # task_embeddings = torch.squeeze(self.t5_text_embeddings[item["task"]])
+            task_data = self.t5_text_embeddings[item["task"]]
+            if isinstance(task_data, np.ndarray):
+                task_embeddings = torch.from_numpy(task_data).squeeze()
+            else:
+                task_embeddings = torch.squeeze(self.t5_text_embeddings[item["task"]])
+            # task_embeddings = torch.from_numpy(self.t5_text_embeddings[item["task"]]).squeeze()
         
         # prepare state and action
         item = self.prepare_action_state(item)

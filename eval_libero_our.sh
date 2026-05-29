c8ac7993874763fe56c989d4ae38f65b231afb74
lzl2040 <2040171586@qq.com>
2026-05-29 07:54:26 +0000

    update eval_path

--- eval_libero_our.sh
+++ eval_libero_our.sh
@@ -1,7 +1,7 @@
 uv run --no-sync --extra cu128 --group libero --python 3.10 \
   python -m cosmos_policy.experiments.robot.libero.run_libero_eval \
     --config cosmos_predict2_2b_480p_libero__inference_only \
-    --ckpt_path "/home/cosmos/.cache/cosmos_policy/libero/ace_cosmos_0521/checkpoints/iter_000040000/model" \
+    --ckpt_path "/home/cosmos/.cache/cosmos_policy/libero/ace_cosmos_0525-world-libero-pt/iter_000030000/model" \
     --config_file cosmos_policy/config/config.py \
     --use_wrist_image True \
     --use_proprio False \
@@ -25,7 +25,7 @@ uv run --no-sync --extra cu128 --group libero --python 3.10 \
     --seed 195 \
     --use_variance_scale False \
     --deterministic True \
-    --run_id_note min-max-wo-second-chkpt28000--5stepAct--seed195--deterministic \
+    --run_id_note 0529-ace-cosmos-pt-chkpt30000--5stepAct--seed195--deterministic \
     --ar_future_prediction False \
     --ar_value_prediction False \
     --use_jpeg_compression False \

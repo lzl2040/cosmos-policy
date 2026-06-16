e42c66e367a01794b2a6202ca8a940f71fde93c9
lzl2040 <2040171586@qq.com>
2026-06-16 04:20:52 +0000

    update world modeling

--- cosmos_policy/datasets/lerobot_dataset_v2.py
+++ cosmos_policy/datasets/lerobot_dataset_v2.py
@@ -1623,11 +1623,11 @@ class MultiDatasetforDistTraining(torch.utils.data.Dataset):
         
         is_world_model_sample = False
         is_value_function_sample = False
-        # p_world_model = 0.5
-        # if random.random() < p_world_model:
-        #     is_world_model_sample = True
-        # else:
-        #     is_world_model_sample = False
+        p_world_model = 0.5
+        if random.random() < p_world_model:
+            is_world_model_sample = True
+        else:
+            is_world_model_sample = False
         
         # del item
         task_id = item["task_index"].item()

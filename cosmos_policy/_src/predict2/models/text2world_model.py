00e13748151ac84cea12076e90e07006a363875a
lzl2040 <2040171586@qq.com>
2026-06-22 16:03:19 +0000

    update 0614_ace_40k for 81

--- cosmos_policy/_src/predict2/models/text2world_model.py
+++ cosmos_policy/_src/predict2/models/text2world_model.py
@@ -163,7 +163,7 @@ class DiffusionModel(ImaginaireModel):
         
         # for b200 81
         # 0614_train_ace: all v21 data, interna1 weight increase, franka decrease
-        # ace_pt_path = "/mnt/pvc/msra-training_data/weights/pt_ace/0614_train_ace/step_6k/mp_rank_00_model_states.pt"
+        ace_pt_path = "/mnt/pvc/msra-training_data/weights/pt_ace/0614_train_ace/step_40k/mp_rank_00_model_states.pt"
         # ace_pt_path = "/mnt/pvc/msra-training_data/weights/pt_ace/0522_ace/step_22k/mp_rank_00_model_states.pt"
         vision_model_name: str = "/mnt/pvc/msra-training_data/weights/siglip2-base-patch16-224/"
         # for b200 03
@@ -171,7 +171,7 @@ class DiffusionModel(ImaginaireModel):
         # 0522_ace(0520 in blob): fix action equal zero bug
         # 0523_ace_add_norm (0523_debug in blob): add layer norm and l2 norm for contrastive embedding
         ## 
-        ace_pt_path = "/mnt/pvc/robo_home/lzl_ckpts/ace_pt_weights/0614_train_ace/mp_rank_00_model_states.pt"
+        # ace_pt_path = "/mnt/pvc/robo_home/lzl_ckpts/ace_pt_weights/0614_train_ace/mp_rank_00_model_states.pt"
         vision_model_name = vision_model_name if os.path.isdir(vision_model_name) else "google/siglip2-base-patch16-224"
         
         

bb8f54a7b66a71cdbac3d68ad054ca7631bef788
lzl2040 <2040171586@qq.com>
2026-08-06 03:59:16 +0000

    support multi node gpu

--- run_b200.sh
+++ run_b200.sh
@@ -1,5 +1,8 @@
 NNODES=1
 NPROC_PER_NODE=2
+NODE_RANK=0
+MASTER_ADDR="127.0.0.1"
+MASTER_PORT=9901
 BATCH_SIZE=4
 DATA_MIX="libero"
 STAGE="finetune"
@@ -102,7 +105,7 @@ uv run --no-sync --extra cu128 --group libero --python 3.10 \
 which ffmpeg
 
 uv run --no-sync --extra cu128 --group libero --python 3.10 \
-  torchrun --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE --master_port=$MASTER_PORT \
+  torchrun --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
   -m cosmos_policy.scripts.train \
   --config=cosmos_policy/config/config.py --job_name=$JOB_NAME -- \
   experiment="cosmos_predict2_2b_480p_libero" \
